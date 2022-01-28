f72ec38ef3a38f5df43689e915c4028dbaf35127
Isti1AA <szilagyii.12d@acsjszki.hu>
2022-01-28 12:52:15 +0100

    Add project files.

---
+++ Opening_weekend/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OpeningWeekend
+{
+    class Program
+    {
+
+        class adat
+        {
+            public string eredetiCim;
+            public string magyarCim;
+            public string bemutato;
+            public string forgalmazo;
+            public int bevetel;
+            public int latogato;
+
+
+            public sor()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+            //eredetiCim;magyarCim;bemutato;forgalmazo;bevel;latogato
+          //  Allied; Szövetségesek; 2016.12.01; UIP; 44341076; 30804
+        }
+    }
+}
